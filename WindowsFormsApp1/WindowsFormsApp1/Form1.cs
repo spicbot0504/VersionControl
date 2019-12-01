9f7ac8ec1986b6029752772284f5ee0cee10c55a
Balázs Vermes <balazs.vermes@stud.uni-corvinus.hu>
2019-12-01 20:33:50 +0100

    UI elkészült

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -15,6 +15,9 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+            label1.Text = Resource1.LastName; 
+            label2.Text = Resource1.FirstName;
+            button1.Text = Resource1.Add;
         }
     }
 }
